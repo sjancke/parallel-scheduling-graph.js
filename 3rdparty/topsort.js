3536bfc20e542deea3e6ccb12a45ed618fe8b298
sjancke <sebastian.jancke@googlemail.com>
2016-01-27 23:21:20 +0100

    parallel scheduling execution order using topological sort

---
+++ 3rdparty/topsort.js
@@ -0,0 +1,50 @@
+/* from https://github.com/marcelklehr/toposort/blob/master/index.js */
+function topsort(edges) {
+  return toposort(uniqueNodes(edges), edges);
+}
+
+function toposort(nodes, edges) {
+  var cursor = nodes.length
+    , sorted = new Array(cursor)
+    , visited = {}
+    , i = cursor
+
+  while (i--) {
+    if (!visited[i]) visit(nodes[i], i, [])
+  }
+
+  return sorted
+
+  function visit(node, i, predecessors) {
+    if(predecessors.indexOf(node) >= 0) {
+      throw new Error('Cyclic dependency: '+JSON.stringify(node))
+    }
+
+    if (visited[i]) return;
+    visited[i] = true
+
+    // outgoing edges
+    var outgoing = edges.filter(function(edge){
+      return edge[0] === node
+    })
+    if (i = outgoing.length) {
+      var preds = predecessors.concat(node)
+      do {
+        var child = outgoing[--i][1]
+        visit(child, nodes.indexOf(child), preds)
+      } while (i)
+    }
+
+    sorted[--cursor] = node
+  }
+}
+
+function uniqueNodes(arr){
+  var res = []
+  for (var i = 0, len = arr.length; i < len; i++) {
+    var edge = arr[i]
+    if (res.indexOf(edge.source.id) < 0) res.push(edge.source.id)
+    if (res.indexOf(edge.target.id) < 0) res.push(edge.target.id)
+  }
+  return res
+}
